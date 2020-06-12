a3e6e4ed8442013c743d992c4c56180be9d9a6d9
PRI91 <66439327+PRI91@users.noreply.github.com>
2020-06-12 14:02:04 -0300

    Update jogo_de_aventura.py

--- jogo_de_aventura.py
+++ jogo_de_aventura.py
@@ -3,11 +3,17 @@
 
 # Chegar a finais diferentes na minha historia, de acordo com as respostas que eu passe para o programa
 
-#Qual é o cenário : Eu estou numa guerra entre duas nações, onde existe 2 lados: LadoA e LadoB. Somente o LadoA irá vencer, e o LadoB irá
+#Qual é o cenário : Eu estou numa guerra entre duas nações, onde existe 2 lados: LadoA e LadoB. Somente o LadoA irá vencer, 
 
-perder! Então eu tenho que tomar as decisões corretas para chegar até a vitória, que somente o lado LadoA conseguira!
+
 class JogoDeAventura:
     def__init__(self):
        self.pergunta1 = 'você nasceu no norte ou sul (s/n?' #sul = LadoA, norte = LadoB
        self.pergunta2 = 'você prefere a espada ou escudo? (espada/escudo)' #espada = Lado1, escudo = Lado2
        self.pergunta3 = 'qual a sua especialidade? (linha de frente/tatico)' # linha de frente = Lado1, tático = Lado2
+
+    def Iniciar(self):
+    resposta1 = input(self.pergunta1)
+    if resposta1 == 'n': 
+        resposta1B = input(self.pergunta2):
+        if resposta1B == 'espada':
