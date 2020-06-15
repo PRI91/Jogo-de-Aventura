4ec48021042fbf6e8a87cb6d4163ecfd26bb1ba5
PRI91 <66439327+PRI91@users.noreply.github.com>
2020-06-14 21:42:44 -0300

    Update jogo_de_aventura.py

--- jogo_de_aventura.py
+++ jogo_de_aventura.py
@@ -3,17 +3,33 @@
 
 # Chegar a finais diferentes na minha historia, de acordo com as respostas que eu passe para o programa
 
-#Qual é o cenário : Eu estou numa guerra entre duas nações, onde existe 2 lados: LadoA e LadoB. Somente o LadoA irá vencer, 
-
-
+# Qual é o cenário : Eu estou numa guerra entre duas nações, onde existe 2 lados: LadoA e LadoB. Somente o LadoA irá vencer,
+#  e o LadoB irá perder! então tenho que tomar as decisões corretas para chegar até a vitória, que somente o LadoA conseguira.
 class JogoDeAventura:
-    def__init__(self):
+    def_init_(self):
        self.pergunta1 = 'você nasceu no norte ou sul (s/n?' #sul = LadoA, norte = LadoB
-       self.pergunta2 = 'você prefere a espada ou escudo? (espada/escudo)' #espada = Lado1, escudo = Lado2
+       self.pergunta2 = 'você prefere a espada ou flexa? (espada/flexa)' #espada = Lado1, flexa = Lado2
        self.pergunta3 = 'qual a sua especialidade? (linha de frente/tatico)' # linha de frente = Lado1, tático = Lado2
+       self.finalHistoria1 = 'Você será um heroi na linha de frente!'
+       self.finalHistoria2 = 'Você sera um heroi protegendo todas as nossas tropas!'
+       self.finalHistoria3 = 'Você irá se sacrificar na batalha!'
+       self.finalHistoria4 = 'Você não é capaz de lutar nessa batalha!'
+
 
     def Iniciar(self):
-    resposta1 = input(self.pergunta1)
-    if resposta1 == 'n': 
-        resposta1B = input(self.pergunta2):
-        if resposta1B == 'espada':
+        resposta1 = input(self.pergunta1)
+        if resposta1 == 'n': 
+            resposta1B = input(self.pergunta2)
+            if resposta1B == 'espada':
+                print(self.finalHistoria1)
+            if resposta1B == 'flexa':
+                print (self.finalHistoria2)    
+        if resposta1 == 's':
+            resposta1B = input(self.pergunta3)
+            if resposta1B == 'linha de frente':
+                print(self.finalHistoria3)
+            if resposta1B == 'tatico':
+                print(self.finalHistoria4)
+
+jogo = JogoDeAventura()
+jogo Iniciar()               
